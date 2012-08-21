66bae1a0e5e5a095c893a18241674598b5847fdb
Bill Baumgartner <william.baumgartner@ucdenver.edu>
2012-08-21 22:16:37 +0000

    Migrating from fileparsers to datasource-fileparsers

---
+++ datasource-fileparsers/src/main/java/edu/ucdenver/ccp/datasource/fileparsers/MultiLineFileRecord.java
@@ -0,0 +1,9 @@
+package edu.ucdenver.ccp.fileparsers;
+
+public abstract class MultiLineFileRecord extends FileRecord {
+
+	public MultiLineFileRecord(long byteOffset) {
+		super(byteOffset);
+	}
+
+}
