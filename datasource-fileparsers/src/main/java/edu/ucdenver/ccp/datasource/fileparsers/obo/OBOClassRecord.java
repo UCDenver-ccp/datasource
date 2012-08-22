551281bfec54bd1e6a6d684af70a0dac6e7bb48d
Bill Baumgartner <william.baumgartner@ucdenver.edu>
2012-08-22 02:01:27 +0000

    Migrating from fileparsers to datasource-fileparsers

---
+++ datasource-fileparsers/src/main/java/edu/ucdenver/ccp/datasource/fileparsers/obo/OBOClassRecord.java
@@ -0,0 +1,26 @@
+package edu.ucdenver.ccp.fileparsers.obo;
+
+import org.geneontology.oboedit.datamodel.OBOClass;
+
+import edu.ucdenver.ccp.datasource.fileparsers.DataRecord;
+
+/**
+ * Simple wrapper for the OBOEdit <code>OBOClass</code> class allowing it to be treated as a
+ * <code>DataRecord</code>.
+ * 
+ * @author bill
+ * 
+ */
+public class OBOClassRecord implements DataRecord {
+
+	private final OBOClass oboClass;
+
+	public OBOClassRecord(OBOClass oboClass) {
+		this.oboClass = oboClass;
+	}
+
+	public OBOClass getOboClass() {
+		return oboClass;
+	}
+
+}
