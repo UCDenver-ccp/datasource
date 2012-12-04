9e3025607b262124983c2d15e3848bfdfb0b3ffa
Bill Baumgartner <william.baumgartner@ucdenver.edu>
2012-12-04 22:04:10 +0000

    commenting out ec number validation -- need to figure out what makes an ec number invalid

--- datasource-identifiers/src/main/java/edu/ucdenver/ccp/datasource/identifiers/ec/EnzymeCommissionID.java
+++ datasource-identifiers/src/main/java/edu/ucdenver/ccp/datasource/identifiers/ec/EnzymeCommissionID.java
@@ -29,14 +29,10 @@
  */
 package edu.ucdenver.ccp.datasource.identifiers.ec;
 
-import org.apache.commons.lang.StringUtils;
-
-import edu.ucdenver.ccp.common.string.RegExPatterns;
-import edu.ucdenver.ccp.common.string.StringUtil;
-import edu.ucdenver.ccp.datasource.identifiers.DataSourceIdentifier;
 import edu.ucdenver.ccp.datasource.identifiers.DataSource;
+import edu.ucdenver.ccp.datasource.identifiers.StringDataSourceIdentifier;
 
-public class EnzymeCommissionID extends DataSourceIdentifier<String> {
+public class EnzymeCommissionID extends StringDataSourceIdentifier {
 
 	public EnzymeCommissionID(String resourceID) {
 		super(resourceID);
@@ -47,17 +43,17 @@ public class EnzymeCommissionID extends DataSourceIdentifier<String> {
 		return DataSource.ENZYME_COMMISSION;
 	}
 
-	@Override
-	public String validate(String ecNumber) throws IllegalArgumentException {
-		String validatedEcNumber = ecNumber;
-		/* remove trailing periods and hyphens*/
-		while (validatedEcNumber.endsWith(".") || validatedEcNumber.endsWith("-") || validatedEcNumber.endsWith(" "))
-			validatedEcNumber = StringUtil.removeLastCharacter(validatedEcNumber);
-		String ecNumberPatternStr = String
-				.format("(EC\\s)?\\d+(\\.%s)?(\\.%s)?(\\.%s)?", RegExPatterns.IS_NUMBER_OR_HYPHEN,
-						RegExPatterns.IS_NUMBER_OR_HYPHEN, RegExPatterns.IS_NUMBER_OR_HYPHEN);
-		if (validatedEcNumber.matches(ecNumberPatternStr))
-			return validatedEcNumber;
-		throw new IllegalArgumentException(String.format("Invalid EC number: %s", ecNumber));
-	}
+//	@Override
+//	public String validate(String ecNumber) throws IllegalArgumentException {
+//		String validatedEcNumber = ecNumber;
+//		/* remove trailing periods and hyphens*/
+//		while (validatedEcNumber.endsWith(".") || validatedEcNumber.endsWith("-") || validatedEcNumber.endsWith(" ")) 
+//			validatedEcNumber = StringUtil.removeLastCharacter(validatedEcNumber);
+//		String ecNumberPatternStr = String
+//				.format("(EC\\s)?\\d+(\\.%s)?(\\.%s)?(\\.%s)?", RegExPatterns.IS_NUMBER_OR_HYPHEN,
+//						RegExPatterns.IS_NUMBER_OR_HYPHEN, RegExPatterns.IS_NUMBER_OR_HYPHEN);
+//		if (validatedEcNumber.matches(ecNumberPatternStr))
+//			return validatedEcNumber;
+//		throw new IllegalArgumentException(String.format("Invalid EC number: %s", ecNumber));
+//	}
 }
