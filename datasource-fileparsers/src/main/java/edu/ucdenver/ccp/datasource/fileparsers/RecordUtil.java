6433e4bec31af848453fe9e38dbdf60baaf8830a
Bill Baumgartner <william.baumgartner@ucdenver.edu>
2014-01-21 22:15:26 +0000

    added a method to return the names of the key record fields

--- datasource-fileparsers/src/main/java/edu/ucdenver/ccp/datasource/fileparsers/RecordUtil.java
+++ datasource-fileparsers/src/main/java/edu/ucdenver/ccp/datasource/fileparsers/RecordUtil.java
@@ -7,7 +7,10 @@ import java.lang.reflect.Field;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Map;
+import java.util.Map.Entry;
 import java.util.Set;
+
+import org.apache.log4j.Logger;
 
 import edu.ucdenver.ccp.common.reflection.PrivateAccessor;
 import edu.ucdenver.ccp.datasource.identifiers.DataSource;
@@ -19,6 +22,8 @@ import edu.ucdenver.ccp.datasource.identifiers.DataSource;
  * 
  */
 public class RecordUtil {
+
+	private static final Logger logger = Logger.getLogger(RecordUtil.class);
 
 	/**
 	 * @param record
@@ -51,12 +56,10 @@ public class RecordUtil {
 	public static String getRecordComment(Class<?> recordClass) {
 		return getRecordAnnotation(recordClass).comment();
 	}
-	
+
 	public static String getRecordLabel(Class<?> recordClass) {
 		return getRecordAnnotation(recordClass).label();
 	}
-	
-	
 
 	/**
 	 * @param recordClass
@@ -94,7 +97,7 @@ public class RecordUtil {
 	public static String getRecordFieldComment(Class<?> recordClass, String fieldName) {
 		return getRecordFieldAnnotation(recordClass, fieldName).comment();
 	}
-	
+
 	public static String getRecordFieldLabel(Class<?> recordClass, String fieldName) {
 		return getRecordFieldAnnotation(recordClass, fieldName).label();
 	}
@@ -105,5 +108,24 @@ public class RecordUtil {
 
 	private RecordUtil() {
 		// this class should not be instantiated
+	}
+
+	/**
+	 * @param class1
+	 * @return
+	 */
+	public static Set<String> getKeyFieldNames(Class<? extends DataRecord> recordCls) {
+		Set<String> keyFieldNames = new HashSet<String>();
+		Map<Field, RecordField> fieldToRecordFieldMap = getFieldToRecordFieldAnnotationsMap(recordCls);
+		for (Entry<Field, RecordField> entry : fieldToRecordFieldMap.entrySet()) {
+			if (entry.getValue().isKeyField()) {
+				keyFieldNames.add("F_" + recordCls.getSimpleName() + "_" + entry.getKey().getName() + "_");
+			}
+		}
+		for (String name : keyFieldNames) {
+			logger.info("Key field name to be ignored by duplicate cache: " + name);
+		}
+		return keyFieldNames;
+
 	}
 }
