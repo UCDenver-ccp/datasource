d5c2f551d40404679a1a3af3cf07847e7e5f79fe
Bill Baumgartner <william.baumgartner@ucdenver.edu>
2015-04-14 22:22:26 +0000

    Migrating from rdfizer to datasource-rdfizer

---
+++ datasource-rdfizer/src/main/java/edu/ucdenver/ccp/datasource/rdfizer/rdf/vocabulary/DC.java
@@ -0,0 +1,51 @@
+/**
+ * 
+ */
+package edu.ucdenver.ccp.rdfizer.vocabulary;
+
+import org.openrdf.model.URI;
+import org.openrdf.model.impl.URIImpl;
+
+import edu.ucdenver.ccp.rdfizer.rdf.RdfNamespace;
+import edu.ucdenver.ccp.rdfizer.rdf.RdfUtil;
+
+/**
+ * Enum representing Dublin Core concepts
+ * 
+ * @author Center for Computational Pharmacology, UC Denver; ccpsupport@ucdenver.edu
+ * 
+ */
+public enum DC {
+
+	ABSTRACT("abstract"),
+	CREATED("created"),
+	CREATOR("creator"),
+	CONTRIBUTOR("contributor"),
+	DATE("date"),
+	EXTENT("extent"),
+	HAS_VERSION("hasVersion"),
+	IDENTIFIER("identifier"),
+	IS_PART_OF("isPartOf"),
+	ISSUED("issued"),
+	LANGUAGE("language"),
+	MEDIUM("medium"),
+	MODIFIED("modified"),
+	PUBLISHED("published"),
+	PUBLISHER("publisher"),
+	RELATION("relation"),
+	RIGHTS("rights"),
+	SOURCE("source"),
+	TITLE("title"),
+	TYPE("type");
+
+	private final String termName;
+
+	private DC(String termName) {
+		this.termName = termName;
+	}
+
+	public URI uri() {
+		return new URIImpl(RdfUtil.createUri(RdfNamespace.DC, termName).toString());
+	}
+
+}
