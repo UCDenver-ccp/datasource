07bb1b1e04d3dfa8b024ef5e7e3906be4d172a77
Bill Baumgartner <william.baumgartner@ucdenver.edu>
2015-04-14 22:24:38 +0000

    Migrating from rdfizer to datasource-rdfizer

---
+++ datasource-rdfizer/src/main/java/edu/ucdenver/ccp/datasource/rdfizer/rdf/vocabulary/RO.java
@@ -0,0 +1,33 @@
+/**
+ * 
+ */
+package edu.ucdenver.ccp.rdfizer.vocabulary;
+
+import org.openrdf.model.URI;
+import org.openrdf.model.impl.URIImpl;
+
+import edu.ucdenver.ccp.rdfizer.rdf.RdfNamespace;
+import edu.ucdenver.ccp.rdfizer.rdf.RdfUtil;
+
+/**
+ * @author Center for Computational Pharmacology, UC Denver; ccpsupport@ucdenver.edu
+ * 
+ */
+public enum RO {
+
+	LOCATED_IN("located_in"),
+	PART_OF("part_of"),
+	HAS_PART("has_part"),
+	HAS_PARTICIPANT("has_participant");
+
+	private final String termName;
+
+	private RO(String termName) {
+		this.termName = termName;
+	}
+
+	public URI uri() {
+		return new URIImpl(RdfUtil.createUri(RdfNamespace.RO, termName).toString());
+	}
+
+}
