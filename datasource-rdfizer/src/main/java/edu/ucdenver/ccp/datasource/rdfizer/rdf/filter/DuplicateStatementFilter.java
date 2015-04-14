a70a09a6bd6345f92cce0942bc408b60a5baa438
Bill Baumgartner <william.baumgartner@ucdenver.edu>
2015-04-14 21:19:21 +0000

    Migrating from rdfizer to datasource

---
+++ datasource-rdfizer/src/main/java/edu/ucdenver/ccp/datasource/rdfizer/rdf/filter/DuplicateStatementFilter.java
@@ -0,0 +1,40 @@
+/**
+ * 
+ */
+package edu.ucdenver.ccp.rdfizer.rdf.filter;
+
+import java.io.IOException;
+
+import org.openrdf.model.Statement;
+import org.openrdf.model.impl.URIImpl;
+
+/**
+ * @author Center for Computational Pharmacology, UC Denver; ccpsupport@ucdenver.edu
+ * 
+ */
+public interface DuplicateStatementFilter {
+
+	/**
+	 * @param stmt
+	 * @return
+	 */
+//	public boolean alreadyObservedFieldUri(URIImpl fieldUri);
+
+	public boolean alreadyObservedRecordUri(URIImpl recordUri);
+	
+	public boolean alreadyObservedStatement(Statement stmt);
+
+	/**
+	 * @return true if the filter is 100% sure no duplicate triples leaked through, false otherwise
+	 */
+	public boolean isLeakProof();
+
+	public void shutdown() throws IOException;
+
+	/**
+	 * @param subRecordUri
+	 * @throws  
+	 */
+	public void logRecordUri(URIImpl subRecordUri);
+
+}
