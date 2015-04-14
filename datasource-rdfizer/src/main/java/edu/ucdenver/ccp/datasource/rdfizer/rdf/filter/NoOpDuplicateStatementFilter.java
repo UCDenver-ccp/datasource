844cf32d14b51968e3f286132131519ce51bbb98
Bill Baumgartner <william.baumgartner@ucdenver.edu>
2015-04-14 21:19:57 +0000

    Migrating from rdfizer to datasource

---
+++ datasource-rdfizer/src/main/java/edu/ucdenver/ccp/datasource/rdfizer/rdf/filter/NoOpDuplicateStatementFilter.java
@@ -0,0 +1,46 @@
+/**
+ * 
+ */
+package edu.ucdenver.ccp.rdfizer.rdf.filter;
+
+import org.openrdf.model.Statement;
+import org.openrdf.model.impl.URIImpl;
+
+/**
+ * @author Center for Computational Pharmacology, UC Denver; ccpsupport@ucdenver.edu
+ * 
+ */
+public class NoOpDuplicateStatementFilter implements DuplicateStatementFilter {
+
+//	@Override
+//	public boolean alreadyObservedFieldUri(URIImpl uri) {
+//		return false;
+//	}
+
+	@Override
+	public boolean alreadyObservedRecordUri(URIImpl uri) {
+		return false;
+	}
+
+	@Override
+	public boolean alreadyObservedStatement(Statement stmt) {
+		return false;
+	}
+
+	@Override
+	public boolean isLeakProof() {
+		return false;
+	}
+
+	@Override
+	public void shutdown() {
+		// do nothing
+	}
+
+	@Override
+	public void logRecordUri(URIImpl subRecordUri) {
+		// do nothing
+	}
+
+
+}
