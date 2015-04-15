ec1c7c16f9d762e88ed599fd95ac953ea5c4f1fb
Bill Baumgartner <william.baumgartner@ucdenver.edu>
2015-04-15 04:44:52 +0000

    Migrating from fileparsers to datasource-fileparsers

---
+++ datasource-fileparsers/src/main/java/edu/ucdenver/ccp/datasource/fileparsers/mirbase/SequenceFeatureQualifierSet.java
@@ -0,0 +1,11 @@
+/**
+ * 
+ */
+package edu.ucdenver.ccp.fileparsers.ebi.embl;
+
+/**
+ * @author Colorado Computational Pharmacology, UC Denver; ccpsupport@ucdenver.edu
+ * 
+ */
+public interface SequenceFeatureQualifierSet {
+}
