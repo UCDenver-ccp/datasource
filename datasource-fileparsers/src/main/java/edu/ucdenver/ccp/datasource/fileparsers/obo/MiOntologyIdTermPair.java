e6e41cffa2410f2ad12e2ddab0301d141f67edde
Bill Baumgartner <william.baumgartner@ucdenver.edu>
2015-04-15 05:10:43 +0000

    Migrating from fileparsers to datasource-fileparsers

---
+++ datasource-fileparsers/src/main/java/edu/ucdenver/ccp/datasource/fileparsers/obo/MiOntologyIdTermPair.java
@@ -0,0 +1,102 @@
+/**
+ * 
+ */
+package edu.ucdenver.ccp.fileparsers.field;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import lombok.EqualsAndHashCode;
+import lombok.ToString;
+
+import org.apache.log4j.Logger;
+
+import edu.ucdenver.ccp.common.reflection.ConstructorUtil;
+import edu.ucdenver.ccp.datasource.fileparsers.Record;
+import edu.ucdenver.ccp.datasource.fileparsers.RecordField;
+import edu.ucdenver.ccp.datasource.identifiers.DataSource;
+import edu.ucdenver.ccp.datasource.identifiers.obo.MolecularInteractionOntologyTermID;
+
+/**
+ * Some databases used MI:0000 as a placeholder and attach a specific term name
+ * to it, e.g. in IRefWeb there is both MI:0000(BIND_Translation) and
+ * MI:0000(unspecified). This record allows for the ID and term name to be
+ * paired.
+ * 
+ * @author Center for Computational Pharmacology, UC Denver;
+ *         ccpsupport@ucdenver.edu
+ * 
+ */
+// @Record(dataSource = DataSource.MI_ONTOLOGY)
+@EqualsAndHashCode
+@ToString
+public abstract class MiOntologyIdTermPair {
+
+	private static final Logger logger = Logger.getLogger(MiOntologyIdTermPair.class);
+
+	private final MolecularInteractionOntologyTermID id;
+	private final String termName;
+
+	/**
+	 * @param id
+	 * @param termName
+	 */
+	public MiOntologyIdTermPair(MolecularInteractionOntologyTermID id, String termName) {
+		super();
+		this.id = id;
+		this.termName = termName;
+	}
+
+	public MiOntologyIdTermPair(MolecularInteractionOntologyTermID id) {
+		super();
+		this.id = id;
+		this.termName = null;
+	}
+
+	/**
+	 * @return the id
+	 */
+	protected MolecularInteractionOntologyTermID getId() {
+		return id;
+	}
+
+	/**
+	 * @return the termName
+	 */
+	protected String getTermName() {
+		return termName;
+	}
+
+	/**
+	 * @param input
+	 * @return parses Strings of the form "MI:0326(protein)" and returns a
+	 *         {@link MiOntologyIdTermPair}
+	 */
+	public static <T extends MiOntologyIdTermPair> T parseString(Class<T> miIdTermPairClass, String input) {
+		Pattern methodIDPattern = Pattern.compile("(MI:\\d+),?\\((.*?)\\)");
+		Matcher m = methodIDPattern.matcher(input);
+		if (m.find()) {
+			return (T) ConstructorUtil.invokeConstructor(miIdTermPairClass.getName(),
+					new MolecularInteractionOntologyTermID(m.group(1)), m.group(2));
+			// return new MiOntologyIdTermPair(new
+			// MolecularInteractionOntologyTermID(m.group(1)),
+			// m.group(2));
+		}
+		methodIDPattern = Pattern.compile("psi-mi:\"(MI:\\d+)\",?\\((.*?)\\)");
+		m = methodIDPattern.matcher(input);
+		if (m.find()) {
+			String miId = m.group(1);
+			String name = (m.group(2).equals("-")) ? null : m.group(2);
+			if (name != null) {
+				return (T) ConstructorUtil.invokeConstructor(miIdTermPairClass.getName(),
+						new MolecularInteractionOntologyTermID(miId), name);
+			}
+			return (T) ConstructorUtil.invokeConstructor(miIdTermPairClass.getName(),
+					new MolecularInteractionOntologyTermID(miId));
+		}
+
+		logger.warn("Unable to extract MiOntologyIdTermPair from: " + input);
+		return null;
+	}
+
+}
