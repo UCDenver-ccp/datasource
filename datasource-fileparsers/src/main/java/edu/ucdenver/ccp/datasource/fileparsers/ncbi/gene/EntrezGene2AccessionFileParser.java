d7ce156d67022b91e417f4177074d9814b7b31df
Bill Baumgartner <william.baumgartner@ucdenver.edu>
2015-04-15 04:06:10 +0000

    Migrating from fileparsers to datasource-fileparsers

---
+++ datasource-fileparsers/src/main/java/edu/ucdenver/ccp/datasource/fileparsers/ncbi/gene/EntrezGene2AccessionFileParser.java
@@ -0,0 +1,88 @@
+/*
+ * Copyright (C) 2009 Center for Computational Pharmacology, University of Colorado School of Medicine
+ * 
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ * 
+ */
+package edu.ucdenver.ccp.fileparsers.ncbi.gene;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+import java.util.zip.GZIPInputStream;
+
+import edu.ucdenver.ccp.common.download.FtpDownload;
+import edu.ucdenver.ccp.common.file.CharacterEncoding;
+import edu.ucdenver.ccp.common.file.reader.Line;
+import edu.ucdenver.ccp.common.file.reader.StreamLineReader;
+import edu.ucdenver.ccp.common.ftp.FTPUtil.FileType;
+import edu.ucdenver.ccp.datasource.fileparsers.SingleLineFileRecordReader;
+import edu.ucdenver.ccp.datasource.identifiers.ncbi.gene.EntrezGeneID;
+import edu.ucdenver.ccp.datasource.identifiers.ncbi.gene.GiNumberID;
+import edu.ucdenver.ccp.datasource.identifiers.ncbi.refseq.RefSeqID;
+import edu.ucdenver.ccp.datasource.identifiers.ncbi.taxonomy.NcbiTaxonomyID;
+import edu.ucdenver.ccp.fileparsers.download.FtpHost;
+
+/**
+ * This class is used to parse the EntrezGene gene2accession file.
+ * 
+ * @author Bill Baumgartner
+ * 
+ */
+public class EntrezGene2AccessionFileParser extends SingleLineFileRecordReader<EntrezGene2AccessionFileData> {
+
+	private static final String HEADER = "#Format: tax_id GeneID status RNA_nucleotide_accession.version RNA_nucleotide_gi protein_accession.version protein_gi genomic_nucleotide_accession.version genomic_nucleotide_gi start_position_on_the_genomic_accession end_position_on_the_genomic_accession orientation assembly mature_peptide_accession.version mature_peptide_gi Symbol (tab is used as a separator, pound sign - start of a comment)";
+
+	public static final String FTP_FILE_NAME = "gene2accession.gz";
+	public static final CharacterEncoding ENCODING = CharacterEncoding.US_ASCII;
+
+	@FtpDownload(server = FtpHost.ENTREZGENE_HOST, path = FtpHost.ENTREZGENE_PATH, filename = FTP_FILE_NAME, filetype = FileType.BINARY)
+	private File gene2accessionFile;
+
+	public EntrezGene2AccessionFileParser(File file, CharacterEncoding encoding) throws IOException {
+		super(file, encoding, null);
+	}
+
+	public EntrezGene2AccessionFileParser(File workDirectory, boolean clean) throws IOException {
+		super(workDirectory, ENCODING, null, null, null, clean);
+	}
+
+	@Override
+	protected StreamLineReader initializeLineReaderFromDownload(CharacterEncoding encoding, String skipLinePrefix)
+			throws IOException {
+		return new StreamLineReader(new GZIPInputStream(new FileInputStream(gene2accessionFile)), encoding,
+				skipLinePrefix);
+	}
+
+	@Override
+	protected String getFileHeader() throws IOException {
+		return readLine().getText();
+	}
+
+	@Override
+	protected String getExpectedFileHeader() throws IOException {
+		return HEADER;
+	}
+
+	@Override
+	protected EntrezGene2AccessionFileData parseRecordFromLine(Line line) {
+		return EntrezGene2AccessionFileData.parseGene2AccessionLine(line);
+	}
+
+}
