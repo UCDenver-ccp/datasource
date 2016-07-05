0052e9123f89a276a5d02496299b42aefa94fa07
bill-baumgartner <bill.baumgartner@gmail.com>
2016-07-05 16:47:29 -0600

    changed encoding to uft8

--- datasource-fileparsers/src/main/java/edu/ucdenver/ccp/datasource/fileparsers/vectorbase/VectorBaseFastaFileRecordReader_aael_transcripts.java
+++ datasource-fileparsers/src/main/java/edu/ucdenver/ccp/datasource/fileparsers/vectorbase/VectorBaseFastaFileRecordReader_aael_transcripts.java
@@ -11,7 +11,7 @@ public class VectorBaseFastaFileRecordReader_aael_transcripts extends VectorBase
 
 	private static final String DOWNLOAD_URL = "https://www.vectorbase.org/download/aedes-aegypti-liverpooltranscriptsaaegl33fagz";
 	private static final String DOWNLOAD_FILE_NAME = "Aedes-aegypti-Liverpool_TRANSCRIPTS_AaegL3.3.fa.gz";
-	private static final CharacterEncoding ENCODING = CharacterEncoding.US_ASCII;
+	private static final CharacterEncoding ENCODING = CharacterEncoding.UTF_8;
 
 	@HttpDownload(url = DOWNLOAD_URL, fileName = DOWNLOAD_FILE_NAME)
 	private File downloadedFile;
