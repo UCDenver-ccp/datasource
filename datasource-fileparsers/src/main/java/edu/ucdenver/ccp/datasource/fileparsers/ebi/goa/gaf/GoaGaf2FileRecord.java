bf01488ae2e55392352198018dd921e633a9bcb8
callahantiff <callahantiff@gmail.com>
2017-06-22 21:51:34 -0400

    Added entries for GOA GAF v2.0 file parser

--- datasource-fileparsers/src/main/java/edu/ucdenver/ccp/datasource/fileparsers/ebi/goa/gaf/GoaGaf2FileRecord.java
+++ datasource-fileparsers/src/main/java/edu/ucdenver/ccp/datasource/fileparsers/ebi/goa/gaf/GoaGaf2FileRecord.java
@@ -1,5 +1,7 @@
 package edu.ucdenver.ccp.datasource.fileparsers.ebi.goa.gaf;
 
+import edu.ucdenver.ccp.datasource.fileparsers.CcpExtensionOntology;
+
 /*
  * #%L
  * Colorado Computational Pharmacology's datasource
@@ -38,7 +40,7 @@ import edu.ucdenver.ccp.datasource.fileparsers.Record;
 import edu.ucdenver.ccp.datasource.fileparsers.format.gaf2.Gaf2FileRecord;
 import edu.ucdenver.ccp.datasource.identifiers.DataSource;
 
-@Record(dataSource = DataSource.GOA)
+@Record(ontClass = CcpExtensionOntology.GOA_GAF_V20_ANNOTATION_RECORD, dataSource = DataSource.GOA)
 public class GoaGaf2FileRecord extends Gaf2FileRecord {
 
 	public GoaGaf2FileRecord(Gaf2FileRecord record) {
